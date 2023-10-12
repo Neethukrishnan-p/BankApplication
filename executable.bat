d1acbe06599c3a8eeeffa37400ced5891d5e21bc
ANORA\neethukrishnan <neethukrishnan@anoralabs.com>
2023-10-12 10:47:56 +0530

    Added the batch file for lauching the server

--- executable.bat
+++ executable.bat
@@ -1,3 +1,14 @@
 @echo off
+
+REM Set the working directory to where start-server.bat is located
+cd "C:\Users\neethukrishnan\BankApplication"
+
+
+REM Install Node.js
+npm install -g serve
+
+REM Install Angular CLI globally
 npm install -g @angular/cli@latest
-ng serve --open
+
+REM Start the Angular development server by calling a separate batch file
+call start-server.bat
---
+++ start-server.bat
@@ -0,0 +1,4 @@
+@echo off
+
+REM Start the Angular development server and open it in the default browser
+ng serve --open
