dd2d92f6f37ed6e1a27f073662bf807d3a590ded
ANORA\neethukrishnan <neethukrishnan@anoralabs.com>
2023-10-12 12:50:10 +0530

    minor update

--- executable.bat
+++ executable.bat
@@ -1,5 +1,9 @@
 @echo off
 
+REM Discover the project directory dynamically by using the location of this batch script
+setlocal enabledelayedexpansion
+set "SCRIPT_DIR=%~dp0"
+set "PROJECT_DIR=%SCRIPT_DIR%..\"
 
 REM Install Node.js
 npm install -g serve
@@ -7,5 +11,6 @@ npm install -g serve
 REM Install Angular CLI globally
 npm install -g @angular/cli@latest
 
-REM Start the Angular development server by calling a separate batch file
+REM Start the Angular development server using the dynamic project directory
+cd "%PROJECT_DIR%"
 call start-server.bat
